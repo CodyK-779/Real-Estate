9e65a08eada24f184a55df810bdc0f739e09bd4a
CodyK <Codyk9627@Gmail.com>
2025-04-23 17:05:41 +0630

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <div className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden" style={{backgroundImage: "url('/header_img.png')"}} id="Home">
+    <div className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden" style={{backgroundImage: "url('../assets/header_img.png')"}} id="Home">
       <Navbar />
       <motion.div
         initial={{opacity: 0, y:100}}
